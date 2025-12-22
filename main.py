b057f6e76ff758973994ad977d1af77547bda7bc
Bhavya <jayagrawal@Bhavyas-MacBook.local>
2025-12-23 01:46:02 +0530

    Backend connected2

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "chatbot-aim.vercel.app",
-        "http://127.0.0.1:5173",
+        # "http://127.0.0.1:5173",
     ],
     allow_credentials=True,
     allow_methods=["*"],
